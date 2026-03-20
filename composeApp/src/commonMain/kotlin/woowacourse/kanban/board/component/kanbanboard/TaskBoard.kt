98c84c36d2a368aa349382f9c4e8e716366ed226
First_woosun <dbdntjs620@gmail.com>
2026-03-21 02:07:52 +0900

    refactor(taskboard): item -> items

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/kanbanboard/TaskBoard.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/kanbanboard/TaskBoard.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +31,7 @@ import woowacourse.kanban.board.model.State
 import woowacourse.kanban.board.model.TaskCardData
 
 @Composable
-fun TaskBoard(taskState: State, taskCards: List<TaskCardData>) {
+fun TaskBoard(taskState: State, taskCards: List<TaskCardData>, modifier: Modifier = Modifier) {
 
     val headerColor = when(taskState){
         State.TODO -> ColorPalette.TODO_HEADER
@@ -45,7 +46,7 @@ fun TaskBoard(taskState: State, taskCards: List<TaskCardData>) {
     }
 
     Column(
-        modifier = Modifier
+        modifier = modifier
             .width(320.dp)
             .fillMaxHeight()
     ) {
@@ -85,14 +86,13 @@ fun TaskBoard(taskState: State, taskCards: List<TaskCardData>) {
                 .fillMaxHeight()
                 .clip(RoundedCornerShape(bottomStart = 10.dp, bottomEnd = 10.dp))
                 .background(bodyColor)
-                .padding(16.dp),
+                .padding(16.dp)
+                .testTag("태스크 카드 칼럼"),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ){
-            item {
-                taskCards.forEach { it ->
-                    TaskCard(it)
-                }
+            items(taskCards.size) {
+                TaskCard(taskCards[it])
             }
         }
 

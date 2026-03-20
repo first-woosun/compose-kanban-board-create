f711e7ec0095b48cdb38f804f1617696496201a7
First_woosun <dbdntjs620@gmail.com>
2026-03-21 02:08:38 +0900

    test(taskboardtest): 테스트 코드 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/taskboard/TaskBoardTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/taskboard/TaskBoardTest.kt
@@ -11,55 +11,64 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performScrollToIndex
+import androidx.compose.ui.test.runComposeUiTest
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.junit.Test
+import woowacourse.kanban.board.component.createtaskcard.ManagerButton
+import woowacourse.kanban.board.component.kanbanboard.TaskBoard
 import woowacourse.kanban.board.model.Manager
 import woowacourse.kanban.board.model.State
 import woowacourse.kanban.board.model.TaskCardData
-import woowacourse.kanban.board.unittest.TaskCardTable
 
-@Composable
-fun TaskBoard(taskState: State, taskCards: List<TaskCardData>){
-    Box(
-        modifier = Modifier
-            .width(320.dp)
-            .fillMaxHeight()
-    ){
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
-            Box(
-                modifier = Modifier.align(alignment = Alignment.Start)
-            ) {
-                Text(
-                    text = taskState.value
-                )
-            }
-        }
-    }
-}
-
-
+@OptIn(ExperimentalTestApi::class)
 class TaskBoardTest {
     @Test
-    fun `입력된 태스크의 카드가 보드에 표시된다`() {
+    fun `입력된 태스크의 카드가 보드에 표시된다`() = runComposeUiTest{
+        val taskCardData = listOf(TaskCardData("title", state = State.TODO, manager = Manager.DINO))
 
+        setContent {
+            TaskBoard(State.TODO, taskCardData)
+        }
+
+        onNodeWithTag("태스크 카드").assertExists()
     }
 
     @Test
-    fun `태스크의 개수가 보드 상단에 표시된다`() {
+    fun `태스크의 개수가 보드 상단에 표시된다`() = runComposeUiTest{
+        val taskCardData = listOf(
+            TaskCardData("title", state = State.TODO, manager = Manager.DINO),
+            TaskCardData("title", state = State.TODO, manager = Manager.DINO),
+        )
 
+        setContent {
+            TaskBoard(State.TODO, taskCardData)
+        }
+
+        onNodeWithText("2").assertExists()
     }
 
     @Test
-    fun `입력된 모든 태스크는 스크롤을 통해 표시 되어야 한다`() {
+    fun `입력된 모든 태스크는 스크롤을 통해 표시 되어야 한다`() = runComposeUiTest{
+        setContent {
+            TaskBoard(State.TODO, mock)
+        }
 
+        onNodeWithTag("태스크 카드 칼럼").performScrollToIndex(4)
     }
 
     companion object {
-        private val mock = List<TaskCardData>(10000) { TaskCardData(title = "${it}", state = State.TODO, manager = Manager.DINO) }
+        private val mock = listOf(
+            TaskCardData(title = "title", state = State.TODO, manager = Manager.DINO),
+            TaskCardData(title = "title", state = State.TODO, manager = Manager.DINO),
+            TaskCardData(title = "title", state = State.TODO, manager = Manager.DINO),
+            TaskCardData(title = "title", state = State.TODO, manager = Manager.DINO),
+            TaskCardData(title = "title", state = State.TODO, manager = Manager.DINO),
+            TaskCardData(title = "title", state = State.TODO, manager = Manager.DINO),
+        )
     }
 }

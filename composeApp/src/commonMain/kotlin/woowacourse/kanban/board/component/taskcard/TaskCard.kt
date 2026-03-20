febf9af6aa3c911d2505b9fc6eb32e7996b0e48a
First_woosun <dbdntjs620@gmail.com>
2026-03-21 02:09:10 +0900

    feat(taskcard): 테스크 태그 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/taskcard/TaskCard.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/taskcard/TaskCard.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.kanban.board.constant.ColorPalette
@@ -29,7 +30,8 @@ fun TaskCard(
 ) {
     Card(
         modifier = modifier
-            .width(286.dp),
+            .width(286.dp)
+            .testTag("태스크 카드"),
         colors = CardDefaults.cardColors(
             containerColor = Color.White,
         ),

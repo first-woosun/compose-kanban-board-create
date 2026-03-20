811fb796c0712efa0142a7cc06f371bc4114c28c
First_woosun <dbdntjs620@gmail.com>
2026-03-21 01:00:33 +0900

    feat(app): 호출 컴포저블 변경

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,9 +1,19 @@
 package woowacourse.kanban.board
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import woowacourse.kanban.board.component.createtaskcard.TaskCardDataInput
+import woowacourse.kanban.board.component.kanbanboard.KanbanBoard
+import woowacourse.kanban.board.model.TaskCardDataInputState
+import woowacourse.kanban.board.model.TaskCardTable
 
 @Composable
 fun App() {
-    TaskCardDataInput()
+    val state = remember { TaskCardDataInputState() }
+    val taskCardTable = remember { TaskCardTable() }
+
+    KanbanBoard(
+        state,
+        taskCardTable
+    )
 }

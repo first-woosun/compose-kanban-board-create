7f821c73afd61481473f0a670c84ae8742a0ac6c
yws <dbdntjs620@gmail.com>
2026-03-23 17:43:31 +0900

    refactor(taskcarddatainput): Dialog 컴포저블 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/createtaskcard/ui/TaskCardDataInput.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/createtaskcard/ui/TaskCardDataInput.kt
@@ -30,6 +30,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import woowacourse.kanban.board.constant.ColorPalette
 import woowacourse.kanban.board.constant.DescriptionConst
 import woowacourse.kanban.board.constant.HeaderAndFooterConst
@@ -51,151 +53,157 @@ fun TaskCardDataInput(
     onCreate: (TaskCardData) -> Unit,
     onCancel: () -> Unit
 ) {
-    Card(
-        modifier = Modifier
-            .width(800.dp)
-            .padding(50.dp)
-            .testTag(TestTags.MODAL_BODY),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White,
-        ),
-    ) {
-        Column(
+    Dialog(
+        onDismissRequest = onCancel,
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+
+    ){
+        Card(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(vertical = 24.dp)
-                .testTag(TestTags.HEADER),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(24.dp),
+                .width(800.dp)
+                .padding(50.dp)
+                .testTag(TestTags.MODAL_BODY),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+            ),
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .padding(horizontal = 24.dp)
-                    .fillMaxWidth(),
-            ) {
-                Text(
-                    text = HeaderAndFooterConst.HEADER_LABEL,
-                    fontSize = 20.sp,
-                    color = Color.Black,
-                    fontWeight = FontWeight.Bold,
-                )
-                Icon(
-                    modifier = Modifier
-                        .clickable {
-                            onCancel()
-                        },
-                    imageVector = Icons.Default.Close,
-                    contentDescription = "닫기",
-                )
-            }
-
-            HorizontalDivider()
-
-            LabelAndContent(
-                label = TitleConst.TITLE_LABEL,
-            ) {
-                TextInput(
-                    value = state.title,
-                    placeholder = TitleConst.TITLE_PLACEHOLDER,
-                    onTextChange = { state.title = it },
-                    singleLine = true,
-                    isError = Title(state.title).isNotValidTitle(),
-                    errorText = TitleConst.TITLE_ERROR,
-                    modifier = Modifier.testTag(TestTags.TITLE_INPUT)
-                )
-            }
-
-            LabelAndContent(
-                label = DescriptionConst.DESCRIPTION_LABEL,
-            ) {
-                TextInput(
-                    value = state.description,
-                    placeholder = DescriptionConst.DESCRIPTION_PLACEHOLDER,
-                    onTextChange = { state.description = it },
-                    modifier = Modifier
-                        .height(200.dp)
-                        .testTag(TestTags.DESCRIPTION_INPUT),
-                )
-            }
-
-            LabelAndContent(
-                label = TagsConst.TAG_LABEL,
-            ) {
-                TextInput(
-                    value = state.tags,
-                    placeholder = TagsConst.TAG_PLACEHOLDER,
-                    onTextChange = { state.tags = it },
-                    singleLine = true,
-                    supportingText = TagsConst.TAG_SUPPORTING,
-                    isError = Tags(state.tags).isNotValidTags(),
-                    errorText = TagsConst.TAG_ERROR,
-                    modifier = Modifier.testTag(TestTags.TAGS_INPUT)
-                )
-            }
-
-            LabelAndContent(
-                label = StateButtonConst.STATE_BUTTON_LABEL,
-                modifier = Modifier.testTag(TestTags.STATE_BTN),
-            ) {
-                State.entries.forEach { option ->
-                    StateButton(
-                        option = option.value,
-                        isSelected = state.selectedState == option,
-                        onClick = { state.selectedState = option },
-                        modifier = Modifier.testTag("${option.value}${TestTags.BTN}")
-                    )
-                }
-            }
-
-            LabelAndContent(
-                label = ProfileButtonConst.PROFILE_BUTTON_LABEL,
-                modifier = Modifier.testTag(TestTags.MANAGER_BTN)
-            ) {
-                Manager.entries.forEach { option ->
-                    ManagerButton(
-                        option = option.value,
-                        isSelected = state.selectedManager == option,
-                        onClick = { state.selectedManager = option },
-                        modifier = Modifier.testTag("${option.value}${TestTags.BTN}"),
-                    )
-                }
-            }
-
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp)
-                    .testTag(TestTags.FOOTER),
+                    .fillMaxSize()
+                    .padding(vertical = 24.dp)
+                    .testTag(TestTags.HEADER),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(24.dp),
             ) {
-                HorizontalDivider()
-                Spacer(modifier = Modifier.height(8.dp))
                 Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier
+                        .padding(horizontal = 24.dp)
                         .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
                 ) {
-                    ActionButton(
-                        containerColor = Color.Transparent,
-                        contentColor = ColorPalette.Gray20,
-                        text = HeaderAndFooterConst.CANCEL_BUTTON,
-                        onClick = {
-                            onCancel()
-                        }
+                    Text(
+                        text = HeaderAndFooterConst.HEADER_LABEL,
+                        fontSize = 20.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
                     )
-
-                    Spacer(modifier = Modifier.width(12.dp))
-
-                    ActionButton(
-                        enabled =  !Title(state.title).isNotValidTitle() && !Tags(state.tags).isNotValidTags() ,
-                        containerColor = ColorPalette.Blue50,
-                        text = HeaderAndFooterConst.CREATE_BUTTON,
-                        onClick = {
-                            onCreate(state.getCard())
-                            state.initializeState()
-                        }
+                    Icon(
+                        modifier = Modifier
+                            .clickable {
+                                onCancel()
+                            },
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "닫기",
                     )
+                }
+
+                HorizontalDivider()
+
+                LabelAndContent(
+                    label = TitleConst.TITLE_LABEL,
+                ) {
+                    TextInput(
+                        value = state.title,
+                        placeholder = TitleConst.TITLE_PLACEHOLDER,
+                        onTextChange = { state.title = it },
+                        singleLine = true,
+                        isError = Title(state.title).isNotValidTitle(),
+                        errorText = TitleConst.TITLE_ERROR,
+                        modifier = Modifier.testTag(TestTags.TITLE_INPUT)
+                    )
+                }
+
+                LabelAndContent(
+                    label = DescriptionConst.DESCRIPTION_LABEL,
+                ) {
+                    TextInput(
+                        value = state.description,
+                        placeholder = DescriptionConst.DESCRIPTION_PLACEHOLDER,
+                        onTextChange = { state.description = it },
+                        modifier = Modifier
+                            .height(200.dp)
+                            .testTag(TestTags.DESCRIPTION_INPUT),
+                    )
+                }
+
+                LabelAndContent(
+                    label = TagsConst.TAG_LABEL,
+                ) {
+                    TextInput(
+                        value = state.tags,
+                        placeholder = TagsConst.TAG_PLACEHOLDER,
+                        onTextChange = { state.tags = it },
+                        singleLine = true,
+                        supportingText = TagsConst.TAG_SUPPORTING,
+                        isError = Tags(state.tags).isNotValidTags(),
+                        errorText = TagsConst.TAG_ERROR,
+                        modifier = Modifier.testTag(TestTags.TAGS_INPUT)
+                    )
+                }
+
+                LabelAndContent(
+                    label = StateButtonConst.STATE_BUTTON_LABEL,
+                    modifier = Modifier.testTag(TestTags.STATE_BTN),
+                ) {
+                    State.entries.forEach { option ->
+                        StateButton(
+                            option = option.value,
+                            isSelected = state.selectedState == option,
+                            onClick = { state.selectedState = option },
+                            modifier = Modifier.testTag("${option.value}${TestTags.BTN}")
+                        )
+                    }
+                }
+
+                LabelAndContent(
+                    label = ProfileButtonConst.PROFILE_BUTTON_LABEL,
+                    modifier = Modifier.testTag(TestTags.MANAGER_BTN)
+                ) {
+                    Manager.entries.forEach { option ->
+                        ManagerButton(
+                            option = option.value,
+                            isSelected = state.selectedManager == option,
+                            onClick = { state.selectedManager = option },
+                            modifier = Modifier.testTag("${option.value}${TestTags.BTN}"),
+                        )
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp)
+                        .testTag(TestTags.FOOTER),
+                ) {
+                    HorizontalDivider()
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End,
+                    ) {
+                        ActionButton(
+                            containerColor = Color.Transparent,
+                            contentColor = ColorPalette.Gray20,
+                            text = HeaderAndFooterConst.CANCEL_BUTTON,
+                            onClick = {
+                                onCancel()
+                            }
+                        )
+
+                        Spacer(modifier = Modifier.width(12.dp))
+
+                        ActionButton(
+                            enabled =  !Title(state.title).isNotValidTitle() && !Tags(state.tags).isNotValidTags() ,
+                            containerColor = ColorPalette.Blue50,
+                            text = HeaderAndFooterConst.CREATE_BUTTON,
+                            onClick = {
+                                onCreate(state.getCard())
+                                state.initializeState()
+                            }
+                        )
+                    }
                 }
             }
         }
